5001733efb3c7fa05c85c90337691a3585586074
Sida14 <164236903+Sida14@users.noreply.github.com>
2024-05-25 17:03:04 +0400

    Add files via upload

---
+++ javascript 1/script.js
@@ -0,0 +1,8 @@
+
+let myName = "Levan sidamonidze"
+let myHobby = "football"
+let yearOfBirth = "2010"
+let currentYear = "2024"
+
+
+console.log ("My name is " + (myName) + ",I am " + (currentYear - yearOfBirth) + " old and my hobby is " + (myHobby))
